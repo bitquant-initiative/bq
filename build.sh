126e0b5b354200303ba11cfdc98c1a5fc78d3a82
Rob Schoening <robschoening@gmail.com>
2025-02-12 23:22:25 -0800

    update

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 if [[ "${CI}" = "true" ]]; then
     GPG_SKIP_OPT="-Dgpg.skip"
-    MAVEN_OPTS="${MAVEN_OPTS} ${GPG_SKIP_OPT}"
+    MAVEN_OPTS="${GPG_SKIP_OPT} -B ${MAVEN_OPTS}"
 fi
 
 echo ./mvnw clean install ${MAVEN_OPTS}
