c2adfc5bfa803428bfe1c553f2267d713312f0e1
Rob Schoening <robschoening@gmail.com>
2025-02-13 04:55:08 -0800

    remove old build.sh

--- bq-base/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-docker build . --no-cache --platform linux/amd64,linux/arm64 -t bq-base -t ghcr.io/bitquant-initiative/bq-base
-
-if [[ "${CI}" = "true" ]]; then
-
-    docker push ghcr.io/bitquant-initiative/bq-base
-
-fi
--- bq-indicators/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-if [[ "${CI}" = "true" ]]; then
-    GPG_SKIP_OPT="-Dgpg.skip"
-fi
-
- mvn -B clean test install ${GPG_SKIP_OPT}
