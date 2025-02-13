40d47b73d2d045c65ebfbcb123bd2331b223a634
Rob Schoening <robschoening@gmail.com>
2025-02-13 09:56:36 -0800

    --no-transfer-progress

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 if [[ "${CI}" = "true" ]]; then
     GPG_SKIP_OPT="-Dgpg.skip"
-    MAVEN_OPTS="${GPG_SKIP_OPT} -B ${MAVEN_OPTS}"
+    MAVEN_OPTS="${GPG_SKIP_OPT} --no-transfer-progress -B ${MAVEN_OPTS}"
 fi
 
 ./mvnw clean install ${MAVEN_OPTS}
