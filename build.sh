43d8277f15713ab9bb23445d7952153c36a39074
Rob Schoening <robschoening@gmail.com>
2025-02-17 08:18:51 -0800

    add pre-build script (#23)

--- build.sh
+++ build.sh
@@ -3,12 +3,14 @@
 set -e
 set -x
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+PROJECT_ROOT=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 
 if [[ "${CI}" = "true" ]]; then
     GPG_SKIP_OPT="-Dgpg.skip"
     MAVEN_OPTS="${GPG_SKIP_OPT} --no-transfer-progress -B ${MAVEN_OPTS}"
+
+    ${PROJECT_ROOT}/.github/workflows/pre-build.sh
 fi
 
 ./mvnw clean install ${MAVEN_OPTS}
